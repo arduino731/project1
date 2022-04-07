db0249b0e8566ceb5f3d00cb0476ca7dd4174402
arduino731 <arduino731@gmail.com>
2022-04-07 17:04:02 -0500

    successfully install both f and b

--- crudApplication/README.md
+++ crudApplication/README.md
@@ -1,3 +1,12 @@
-# project1
+# CRUD APPLICATION
 
-## Hello world
+
+https://github.com/facebook/create-react-app
+Install React apps with no build configuratio
+$ npx create-react-app . 
+
+To start React applicaiton on client folder
+$ npm start 
+
+To start nodemon application on server folder 
+$ npm run devStart
--- crudApplication/client/src/App.js
+++ crudApplication/client/src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Hello World
+    <div className="App">
+      <h1>Hello World</h1>
+
+      <label> Movie Name</label>
+      <input type="text" name="movieName" />
+      <label>Review</label>
+      <input type="text" name="review" />
+
+      <button>Submit</button>
     </div>
   );
 }
